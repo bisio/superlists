19a2e63917d83e8dfa3f14794794bfd3322104c9
Andrea Bisognin <andrea.bisognin@gmail.com>
2016-04-02 12:09:12 +0200

    adjusted urls and views

--- lists/views.py
+++ lists/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from lists.models import Item
 
 def home_page(request):
-    if request.method=='POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return HttpResponseRedirect(redirect_to='/lists/the-only-list-in-the-world/')
     return render(request,'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request,'list.html',{'items':items}) 
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
+
